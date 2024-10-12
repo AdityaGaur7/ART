ba11a2d2654faa550c4b60f4dc7afbc85d428fe8
AdityaGaur7 <adgaur027@gmail.com>
2024-10-13 01:10:34 +0530

    new arts added

--- src/app/Artist/page.jsx
+++ src/app/Artist/page.jsx
@@ -34,8 +34,8 @@ export default function Arist() {
       <div className="center">
         <h1>Artist</h1>
         <p>It's me Anushka R.Gour</p>
-        <img src="https://firebasestorage.googleapis.com/v0/b/test1-80c19.appspot.com/o/image%2FArtist.jpg?alt=media&token=bc81a708-5b1f-4450-9f0a-0adaea8bed14" style={{margin:'auto',BorderRadius: '10px'}} alt="" />
-        <h4>Hope you will like my drawings</h4>
+        <img src="https://firebasestorage.googleapis.com/v0/b/test1-80c19.appspot.com/o/image%2FAnu.jpg?alt=media&token=650ba597-e31d-42b4-a77c-d2695d28f73f" style={{margin:'auto',BorderRadius: '10px'}} alt="" />
+        <h4>Hope you will like my artworks </h4>
       </div>
     </div>
   )
